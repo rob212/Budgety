d672b9f43c8534abfd638abee27a8441fff1ac70
Rob McBryde <robertmcbryde83@gmail.com>
2020-02-17 13:22:12 +0000

    addition of ability to add and income or expense item to the data structure

--- app.js
+++ app.js
@@ -1,7 +1,59 @@
 
 // BUDGET CONTROLLER
 var budgetController = (function() {
+
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = { 
+        allItems: {
+            exp: [],
+            inc: []
+        }, 
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
  
+    return {
+        addItem: function(type, description, value) {
+            var newItem, ID;
+
+            // Create a new ID
+            if(data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // Create newItem based on 'inc' or 'exp' type
+            if(type === 'exp') {
+                newItem = new Expense(ID, description, value); 
+            } else if(type === 'inc') {
+                newItem = new Income(ID, description, value);
+            }
+
+            // Push it to data structure
+            data.allItems[type].push(newItem);
+
+            // Return the new element
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    };
 })();
 
 
@@ -51,10 +103,12 @@ var controller = (function(budgetCtrl, UICtrl) {
     }
 
     var ctrlAddItem = function() {
+        var input, newItem
         // 1. Get the field input data from description text field
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
 
         // 2. Add the item to the budget calculator
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
 
